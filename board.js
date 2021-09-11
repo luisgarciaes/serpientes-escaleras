094968f019c271c320ef2436f4c17051c21c7417
Zaddoz Garcia <zaddozgl@gmail.com>
2021-09-10 23:53:13 -0500

    fixed functionality and added final HTML info

--- board.js
+++ board.js
@@ -1,10 +1,9 @@
 export default class Board {
     constructor(){
-        let _casillas = new Array(100)
-        let _startSnakes = [98,95,93,87,64,62,56,49,48,16]
-        let _endSnakes = [78,75,73,24,60,19,53,11,26,6]
-        let _startLadders = [1,4,9,21,28,36,51,71,80]
-        let _endLadders = [38,14,31,42,84,44,67,91,100]
-        console.log(_startLadders.length)
+        this._startSnakes = [98,95,93,87,64,62,56,49,48,16]
+        this._endSnakes = [78,75,73,24,60,19,53,11,26,6]
+        this._startLadders = [1,4,9,21,28,36,51,71,80]
+        this._endLadders = [38,14,31,42,84,44,67,91,100]
+        
     }
 }
--- main.js
+++ main.js
@@ -1,73 +1,85 @@
 import Dice from './dice.js';
 import Board from './board.js';
 import Player from './player.js';
-
-console.log("what")
 class App{
     constructor(){
         this._button = document.getElementById("anuime")
-        this._button.addEventListener("click", this.startGame);
+        document.addEventListener("DOMContentLoaded", this.startGame);
         this._board = new Board();
         this._player1 = new Player("Luis");
         this._player2 = new Player("Gerardo");
         this._dice = new Dice;
-        this._ladderLength = Board._startLadders
-        this._snakeLength = Board._startLadders
         
     }   
     checkSnakes = (x) =>{
-        let length = this._snakeLength
-        for (let i = 0; i < length; i++) {  
+        let result = -1;
+        for (let i = 0; i < this._board._startSnakes.length; i++) {
             if (x === this._board._startSnakes[i]) {  
-                return  this._board._startSnakes[i]
+            result = this._board._startSnakes[i]
             }else{
-                return -1;
+                
             }
         }
+        return result;
     }
     checkLadders = (x) =>{
-        console.log(this._board)
-        let length = this._ladderLength;
-        for (let i = 0; i < length; i++) {
+        let result = -1;
+        for (let i = 0; i < this._board._startLadders.length; i++) {
             if (x === this._board._startLadders[i]) {  
-                return  this._board._startLadders[i]
+            result = this._board._startLadders[i]
             }else{
-                return -1;
+                
             }
+        }
+        return result;
+    }
+    checkWinner = (x,x2) =>{
+        if(x._position>x2._position){
+            return x._name + " es el ganador!"
+        }else{
+            return x2._name + " es el ganador!"
         }
     }
     startGame = () =>{
         while (this._player1._position < 100 && this._player2._position < 100) {
+            
+
             this._player1._move(this._dice.roll());
             if(this.checkLadders(this._player1._position)!=-1){
-                console.log(this._player1._name + " encontró una escalera y subió a " + this.checkLadders(this._player1._position))
-                this._player1._position = this.checkLadders(this._player1._position)
+                let pos = this._board._endLadders[this._board._startLadders.indexOf(this.checkLadders(this._player1._position))];
+                console.log(this._player1._name + " encontró una escalera en " + this._player1._position + " y subió a " + pos)
+                this._player1._position = pos;
             }else{
-                return;
+                
             }
             if(this.checkSnakes(this._player1._position)!=-1){
-                console.log(this._player1._name + " encontró una serpiente y bajó a " + this.checkLadders(this._player1._position))
-                this._player1._position = this.checkSnakes(this._player1._position)
+                let pos = this._board._endSnakes[this._board._startSnakes.indexOf(this.checkSnakes(this._player1._position))];
+                console.log(this._player1._name + " encontró una serpiente en " + this._player1._position + " y bajó a " + pos)
+                this._player1._position = pos;
             }else{
-                return;
+                
             }
             console.log(this._player1._name +" se encuentra en " + this._player1._position);
+
+            
             this._player2._move(this._dice.roll());
             if(this.checkLadders(this._player2._position)!=-1){
-                console.log(this._player2._name + " encontró una escalera y subió a " + this.checkLadders(this._player2._position))
-                this._player2._position = this.checkLadders(this._player2._position)
+                let pos = this._board._endLadders[this._board._startLadders.indexOf(this.checkLadders(this._player2._position))];
+                console.log(this._player2._name + " encontró una escalera en " + this._player2._position + " y subió a " + pos)
+                this._player2._position = pos;
             }else{
-                return;
+                
             }
             if(this.checkSnakes(this._player2._position)!=-1){
-                console.log(this._player2._name + " encontró una serpiente y bajó a " + this.checkLadders(this._player2._position))
-                this._player2._position = this.checkSnakes(this._player2._position)
+                let pos = this._board._endSnakes[this._board._startSnakes.indexOf(this.checkSnakes(this._player2._position))];
+                console.log(this._player2._name + " encontró una serpiente en " + this._player2._position + " y bajó a " + pos)
+                this._player2._position = pos;
             }else{
-                return;
+                
             }
             console.log(this._player2._name +" se encuentra en " +this._player2._position);
         }
-
+        console.log(this.checkWinner(this._player1,this._player2))
     }
 }
 new App;
